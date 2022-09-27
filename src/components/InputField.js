9d90990113f10945d018f2bb6af599874239bf4a
hoanganhfuego <hoanganhjalai12@gmail.com>
2022-09-27 14:28:24 +0700

    v2

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -3,7 +3,6 @@ import InputEdit from './inputs/InputEdit';
 import InputSearch from './inputs/InputSearch';
 import { useSelector } from "react-redux";
 
-
 export default function InputField(){
     const editChoose = useSelector(state => state.listTodo.isEditChoose.correct)
     const searchChoose = useSelector(state => state.listTodo.search.correct)
